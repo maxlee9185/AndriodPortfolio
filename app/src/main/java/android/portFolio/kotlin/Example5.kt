b037162b6711921a6086cfe698e1b67c24a204af
seonghyunLee <maxlee9185@naver.com>
2023-03-18 17:47:36 +0900

    when, in, expression

---
+++ app/src/main/java/android/portFolio/kotlin/Example5.kt
@@ -0,0 +1,44 @@
+package android.portFolio.kotlin
+
+fun main(){
+
+    max(10,2);
+    isHoliday("수")
+    isHoliday("토")
+}
+
+//fun max(a:Int, b:Int){
+//    var result= if(a>b) {
+//        println(a)
+//    }
+//    else{
+//        println(b)
+//    }
+//    println(result)
+//}
+
+fun max(a:Int, b:Int){
+    var result=if(a>b)a else b
+    println(result)
+}
+
+fun isHoliday(dayOfWeek:String){
+    var result= when(val day=dayOfWeek){
+        "월","화","수","목","금" ->true
+        "토","일"->if(day=="토") "좋아" else "너무좋아"
+        else->"안좋아"
+    }
+    println(result)
+}
+
+
+fun isHoliday2(dayOfWeek:Any){
+    var result= when(val day=dayOfWeek){
+        "월","화","수","목","금" ->true
+        "토","일"->if(day=="토") "좋아" else "너무좋아"
+        in 2..4 -> {}
+        in listOf(1,3,4) -> {}
+        else->"안좋아"
+    }
+    println(result)
+}
