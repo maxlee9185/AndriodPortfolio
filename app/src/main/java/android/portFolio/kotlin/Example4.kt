a6a72c352f58ee4a06187613814a1c272190596d
seonghyunLee <maxlee9185@naver.com>
2023-03-18 17:06:44 +0900

    class with overriding, open, init, this, and constructor

--- app/src/main/java/android/portFolio/kotlin/Example4.kt
+++ app/src/main/java/android/portFolio/kotlin/Example4.kt
@@ -1,2 +1,24 @@
 package android.portFolio.kotlin
 
+fun main(){
+    val user = person("이성현", age=30)
+    //user.age=29
+    user.job = "Free"
+    println(user.name)
+    //println(user.age)
+    val user2 = kid(name="최지수",28, "Dancer")
+    println(user2.job)
+}
+open class person (open var name : String, private var age : Int, open var job : String="Samsung")
+
+class kid(override var name: String, age: Int, override var job:String): person(name, age, job){
+    var gender: String = "female"
+
+    init{
+        println("initializing...")
+    }
+    //secondary constructor
+    constructor(name:String, age:Int, job:String, gender:String): this(name,age,job){
+        this.gender=gender
+    }
+}
