c9fb48f963999ad528329e288100814ce4b524e3
seonghyunLee <maxlee9185@naver.com>
2023-03-18 16:31:24 +0900

    difference between value and variable

---
+++ app/src/main/java/android/portFolio/kotlin/Example3.kt
@@ -0,0 +1,18 @@
+package android.portFolio.kotlin
+
+fun main(){
+
+    val a : Int = 3
+    var b : Int =5
+
+    //a=3 value cannot be changed
+    b=5 //variable can be changed
+
+    val name = "이성현" //value can assume the type
+    var nickname = "나는 성현"
+    //nickname = 3 cannot change to Inter when is assumed as String first.
+
+
+
+
+}
